22709969a4c99e9b9a74ebf08f357bb359f9f37b
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-08-28 14:18:08 +0700

    Added end_date into Question
    Added end_date into Question attribute

--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    end_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.question_text
@@ -19,7 +20,7 @@ class Question(models.Model):
     )
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return self.pub_date <= now and (self.end_date is None or now <= self.end_date)
 
 
 class Choice(models.Model):
