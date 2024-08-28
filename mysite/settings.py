e422687701e471ac822f5e3240c6a480f81edcc3
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-08-28 21:48:02 +0700

    Fix some issues that causes some error about config
    errors are in previous commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, Csv
+from decouple import Config, Csv, RepositoryEnv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+config = Config(RepositoryEnv('.env.production'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
