25da4b8db90579735fbc31fbca033744767a4617
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-09-03 21:25:52 +0700

    implement one user one vote

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 from django.contrib.auth.decorators import login_required
 
 
@@ -19,6 +19,7 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
+    """Display choices and question of a poll and allow voting"""
     model = Question
     template_name = "polls/question_detail.html"
 
@@ -54,10 +55,22 @@ def vote(request, question_id):
                 "error_message": "You didn't select a choice.",
             },
         )
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    # Reference to the current user
+    current_user = request.user
+    # Check if the user has already voted for this question
+    try:
+        user_vote = Vote.objects.get(
+            user=current_user, choice__question=question)
+        # Update the user's vote to the new choice
+        user_vote.choice = selected_choice
+        user_vote.save()
+        messages.success(
+            request, f"Your vote was changed to '{selected_choice.choice_text}'.")
+    except Vote.DoesNotExist:
+        # If the user hasn't voted yet, create a new vote
+        Vote.objects.create(user=current_user, choice=selected_choice)
+        messages.success(
+            request, f"You voted for '{selected_choice.choice_text}'.")
+
+    # Redirect to the results page
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
