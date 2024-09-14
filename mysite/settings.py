530390bfc22a515a443ad25741d3e6c7e27e6125
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-09-14 22:22:45 +0700

    fix static url
    fix static url

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ LOGGING = {
     },
     'handlers': {
         'console': {
-            'level': 'WARNING',
+            'level': 'ERROR',
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
         },
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
